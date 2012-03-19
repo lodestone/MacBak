57664bf6f9920a8c2af56b2db5a09c8a589a49cd
Werner Gillmer <werner.gillmer@gmail.com>
2012-03-19 15:51:05 +0200

    last commit before going home

--- MacBak.sh
+++ MacBak.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
-# Does a rsync backup over ssh
+# Does a rsync backup over ssh. Key access must be enabled and working.
+# The BACKUP_PATH must exsist on the server where the backups is going to.
 # Schedule this script with cron/launchd if you want.
 # Coded by Werner Gillmer <werner.gillmer@gmail.com>
 
 BACKUP_SERVER="10.0.0.15"
+BACKUP_PATH="/home/wernerg/backups/macbookpro"
 AVAIL_CHECK=`nmap $BACKUP_SERVER -p 22 | awk '/open/ {print $2}'`
+USERNAME="wernerg"
 
 if [ -z $AVAIL_CHECK ]; then
   echo "$BACKUP_SERVER is not availible"
 else
   echo "Starting backup to $BACKUP_SERVER"
-  rsync --stats -a --progress /Users/wernergillmer/Documents -e ssh wernerg@$BACKUP_SERVER:/home/wernerg/backups/macbookpro
-  rsync --stats -a --progress /Users/wernergillmer/Code -e ssh wernerg@$BACKUP_SERVER:/home/wernerg/backups/macbookpro
-  rsync --stats -a --progress /Users/wernergillmer/Pictures -e ssh wernerg@$BACKUP_SERVER:/home/wernerg/backups/macbookpro
+  # Do something usefull with --stats!!
+  rsync --stats -a --progress /Users/wernergillmer/Documents -e ssh $USERNAME@$BACKUP_SERVER:$BACKUP_PATH
+  rsync --stats -a --progress /Users/wernergillmer/Code -e ssh $USERNAME@$BACKUP_SERVER:$BACKUP_PATH
+  rsync --stats -a --progress /Users/wernergillmer/Pictures -e ssh $USERNAME@$BACKUP_SERVER:$BACKUP_PATH
+
+
+  #########
+  # Do some sort of DEFAULT_ALERT, thing here.
 
   # Dialog box to alert backups was done.
   # Makes finder icon bounch and on click shows dialog box.
