d20e98f440e848fff0acbdb50ce7e46b669765b2
Werner Gillmer <werner.gillmer@gmail.com>
2012-04-12 15:13:54 +0200

    Starting with daemon code

--- MacBak.rb
+++ MacBak.rb
@@ -49,26 +49,27 @@ def syncNow
 	 end	
 end
 
-### Start of main
-# Check for configuration file
-@confFile = File.dirname(File.expand_path(__FILE__)) + '/macbak.cnf'
-	if File.exist?("#{@confFile}")
-		config = YAML::load(File.open(@confFile))
-			@backupServer = config['BACKUP_SERVER']
-			@username = config['USERNAME']
-			@alert = config['ALERT']
-			@sshKey = config['SSH_KEY']
-			@backupType = config['BACKUP_TYPE']
-			@emailAddress = config['EMAIL_ADDRESS']
-			@backupPath = config['BACKUP_PATH']
-			@backupList = config['BACKUP_LIST']
+# Main worker function
+def main
+	# Check for configuration file
+	@confFile = File.dirname(File.expand_path(__FILE__)) + '/macbak.cnf'
+		if File.exist?("#{@confFile}")
+			config = YAML::load(File.open(@confFile))
+				@backupServer = config['BACKUP_SERVER']
+				@username = config['USERNAME']
+				@alert = config['ALERT']
+				@sshKey = config['SSH_KEY']
+				@backupType = config['BACKUP_TYPE']
+				@emailAddress = config['EMAIL_ADDRESS']
+				@backupPath = config['BACKUP_PATH']
+				@backupList = config['BACKUP_LIST']
 
-			# Handle "size" command line argument.
-			# Passing size gives the size of your dirs you
-			# want to backup back and then exit
-      if ARGV[0] == 'size'
-      	puts "Calculating..."
-      	@backupList.each do |dir|
+				# Handle "size" command line argument.
+				# Passing size gives the size of your dirs you
+				# want to backup back and then exit
+   	   if ARGV[0] == 'size'
+   	  	 	puts "Calculating..."
+ 		     	@backupList.each do |dir|
       		# Security issue? It's possible to make one of
       		# the dirs in BACKUP_LIST in the conf file
       		# a command like "; rm -Rf /" and that will get parsed
@@ -84,28 +85,39 @@ end
 		Process.exit
 	end
 
-# Check if the backup server is available
-ssh = SSHTest.new
-if ssh.test(@backupServer,@username,@sshKey) == false
-	alertMessage( "ERROR : ssh failed")
-	Process.exit
-else
-  # Check for run file. The run file get's used to
-  # stop MacBak from starting if another instance is
-  # still running
-  @runFile = '/tmp/.macbak.run'
-  if File.exist?(@runFile)
-    puts "Another instance of MacBak is already running"
+	# Check if the backup server is available
+	ssh = SSHTest.new
+	if ssh.test(@backupServer,@username,@sshKey) == false
+		alertMessage( "ERROR : ssh failed")
 		Process.exit
 	else
-		# Create a new file and continue running
-		File.open(@runFile,"w") {}
+	  # Check for run file. The run file get's used to
+	  # stop MacBak from starting if another instance is
+ 	 # still running
+ 	 @runFile = '/tmp/.macbak.run'
+ 	 if File.exist?(@runFile)
+ 	   puts "Another instance of MacBak is already running"
+			Process.exit
+		else
+			# Create a new file and continue running
+			File.open(@runFile,"w") {}
 	end
 
-	# Everything tested 100% do the backups now
-  syncNow
-  # remove the run file now, so that MacBak will
-  # run on the next execute.
-	File.delete(@runFile) 
-end	
+		# Everything tested 100% do the backups now
+ 	 syncNow
+ 	 # remove the run file now, so that MacBak will
+ 	 # run on the next execute.
+		File.delete(@runFile) 
+	end	
+end
 
+def daemonMe
+	`data >> /tmp/test.out`
+end
+
+# Start of main program
+#main
+Daemons.daemonize
+loop {
+	daemonMe
+}
